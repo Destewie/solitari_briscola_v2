8b9bf2b8813d46674912af61134e3db79f4daf31
Federico De Santi <federico.ds25@gmail.com>
2023-08-26 01:07:12 +0200

    fatto il gioco del 1 2 3

--- poeri_solitario.py
+++ poeri_solitario.py
@@ -1,9 +1,21 @@
-from game_essentials import Carta, Mazzo
+from game_essentials import Carta, Mazzo, MazzoFinitoError
 
 class PoeriSolitario:
     def __init__(self):
         self.mazzo = Mazzo()
 
     def setup(self):
-        
         self.mazzo.mescola()
+
+    def gioca(self):
+        self.setup()
+        contatore = 0
+
+        while True:
+            try:
+                contatore = (contatore % 3) + 1  # 1, 2, 3, 1, 2, 3, ...
+                carta = self.mazzo.pesca()
+                if(contatore == carta.valore):
+                    return False
+            except MazzoFinitoError:
+                return True
